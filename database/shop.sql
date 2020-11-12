7965ddc989609ddb47d758a27230c01a6e5f7002
pamungkas93 <dhutadhuta4@gmail.com>
2020-11-12 12:24:28 +0700

    db shop

--- database/shop.sql
+++ database/shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 12 Nov 2020 pada 06.13
+-- Waktu pembuatan: 12 Nov 2020 pada 06.21
 -- Versi server: 10.4.10-MariaDB
 -- Versi PHP: 7.3.12
 
